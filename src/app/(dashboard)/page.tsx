1375e882049ea3c7108d503001c7cc8fbee1e1d8
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 15:19:37 +0600

    feat: CreateTransactionDialog

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import prisma from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import CreateTransactionDialog from './_components/CreateTransactionDialog';
 
 const Page = async () => {
   const user = await currentUser();
@@ -21,18 +22,28 @@ const Page = async () => {
         <div className="container flex flex-wrap justify-between items-center gap-6 py-8">
           <p className="font-bold text-3xl">Hello, {user.firstName}! 👋</p>
           <div className="flex items-center gap-3">
-            <Button
-              variant="secondary"
-              className="border-emerald-500 bg-emerald-950 text-white rounded-md cursor-pointer hover:bg-emerald-800"
-            >
-              New income 🤑
-            </Button>
-            <Button
-              variant="secondary"
-              className="border-rose-500 bg-rose-950 text-white rounded-md cursor-pointer hover:bg-rose-800"
-            >
-              New expense 😤
-            </Button>
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant="secondary"
+                  className="border-emerald-500 bg-emerald-950 text-white rounded-md cursor-pointer hover:bg-emerald-800"
+                >
+                  New income 🤑
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant="secondary"
+                  className="border-rose-500 bg-rose-950 text-white rounded-md cursor-pointer hover:bg-rose-800"
+                >
+                  New expense 😤
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>

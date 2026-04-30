8f5e992ade43e6460eeab2e220ed4fb03c270307
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 15:11:36 +0600

    fix: style

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import prisma from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
@@ -18,7 +19,21 @@ const Page = async () => {
     <div className="h-full bg-background">
       <div className="border-b bg-card">
         <div className="container flex flex-wrap justify-between items-center gap-6 py-8">
-<p className='font-bold text-3xl'>Hello, {user.firstName}! 👋</p>
+          <p className="font-bold text-3xl">Hello, {user.firstName}! 👋</p>
+          <div className="flex items-center gap-3">
+            <Button
+              variant="secondary"
+              className="border-emerald-500 bg-emerald-950 text-white rounded-md cursor-pointer hover:bg-emerald-800"
+            >
+              New income 🤑
+            </Button>
+            <Button
+              variant="secondary"
+              className="border-rose-500 bg-rose-950 text-white rounded-md cursor-pointer hover:bg-rose-800"
+            >
+              New expense 😤
+            </Button>
+          </div>
         </div>
       </div>
     </div>

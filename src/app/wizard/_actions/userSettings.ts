9e9d754f3e0670b38d7f0d293141e358bf10cd47
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 13:09:24 +0600

    feat: chema

---
+++ src/app/wizard/_actions/userSettings.ts
@@ -0,0 +1,1 @@
+export async function UpadateUserCurrency(currency: string){}
--- src/components/CurrencyComboBox.tsx
+++ src/components/CurrencyComboBox.tsx
@@ -8,18 +8,39 @@ import {
   ComboboxItem,
   ComboboxList,
 } from '@/components/ui/combobox';
+import { UserSettings } from '@/generated/client';
 import { Currencies } from '@/lib/currencies';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import SkeletonWrapper from './SkeletonWrapper';
 
 export function CurrencyComboBox() {
-  const userSettings = useQuery({
+  const [value, setValue] = useState<string>("");
+
+  const userSettings = useQuery<UserSettings>({
     queryKey: ['userSettings'],
     queryFn: () => fetch('/api/user-settings').then((res) => res.json()),
   });
+
+  useEffect(() => {
+    if (!userSettings.data) return;
+
+    const userCurrency = Currencies.find(
+      (currency) => currency.value === userSettings.data.currency
+    );
+
+    if (userCurrency) {
+      setValue(userCurrency.value); // ✅ туура
+    }
+  }, [userSettings.data]);
+
   return (
     <SkeletonWrapper isLoading={userSettings.isFetching}>
-      <Combobox items={Currencies}>
+      <Combobox
+        items={Currencies}
+        value={value}
+        onValueChange={(val) => setValue(val ?? "")}
+      >
         <ComboboxInput placeholder="Set currency" />
         <ComboboxContent>
           <ComboboxEmpty>No items found.</ComboboxEmpty>

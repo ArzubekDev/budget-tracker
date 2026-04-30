25d9e334b96c41377396a9fcf07718d86bae6a8a
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 15:30:40 +0600

    feat: transaction.ts

--- src/app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ src/app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { TransactionType } from '@/lib/types';
+import { cn } from '@/lib/utils';
 import { ReactNode } from 'react';
 
 interface Props {
@@ -13,7 +14,11 @@ const CreateTransactionDialog = ({ trigger, type }: Props) => {
   return (
     <Dialog>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
-      
+      <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Create a new <span className={cn('m-1', type === 'income' ? "text-emerald-500" : "text-rose-500")}>{type}</span> transaction</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   );
 };

f6b3e1edb059333d83b8173f8ba9463150d28c91
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 10:13:39 +0600

    feat: prisma

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,5 +8,11 @@ const Logo = () => {
     </a>
   );
 };
-
+export const LogoMobile = () => {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <p className="bg-linear-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight text-transparent">BudgetTracker</p>
+    </a>
+  );
+};
 export default Logo;

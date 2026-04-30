a7573d3531824aa67f289b49b5bb5945eac0c2d5
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 17:31:06 +0600

    fix: CategoryPicker

--- src/app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ src/app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -7,9 +7,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { 
-  Form
-} from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { TransactionType } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import {
@@ -20,6 +26,7 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ReactNode } from 'react';
 import { useForm } from 'react-hook-form';
+import CategoryPicker from './CategoryPicker';
 
 interface Props {
   trigger: ReactNode;
@@ -48,7 +55,52 @@ const CreateTransactionDialog = ({ trigger, type }: Props) => {
           </DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form className="space-y-4"></form>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={''} {...field} />
+                  </FormControl>
+                  <FormDescription>Transaction description (optional)</FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                    />
+                  </FormControl>
+                  <FormDescription>Transaction amount (required)</FormDescription>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between gap-2">
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <CategoryPicker type={type}/>
+                    </FormControl>
+                    <FormDescription>Select a category for this transaction</FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
         </Form>
       </DialogContent>
     </Dialog>

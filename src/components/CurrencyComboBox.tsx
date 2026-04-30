a84ba6037279407c1a4e5a60c125967752affc4b
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 12:49:16 +0600

    feat: SkeletonWrapper

--- src/components/CurrencyComboBox.tsx
+++ src/components/CurrencyComboBox.tsx
@@ -9,21 +9,29 @@ import {
   ComboboxList,
 } from '@/components/ui/combobox';
 import { Currencies } from '@/lib/currencies';
+import { useQuery } from '@tanstack/react-query';
+import SkeletonWrapper from './SkeletonWrapper';
 
 export function CurrencyComboBox() {
+  const userSettings = useQuery({
+    queryKey: ['userSettings'],
+    queryFn: () => fetch('/api/user-settings').then((res) => res.json()),
+  });
   return (
-    <Combobox items={Currencies}>
-      <ComboboxInput placeholder="Set currency" />
-      <ComboboxContent>
-        <ComboboxEmpty>No items found.</ComboboxEmpty>
-        <ComboboxList>
-          {(item) => (
-            <ComboboxItem key={item.value} value={item.value}>
-              {item.label}
-            </ComboboxItem>
-          )}
-        </ComboboxList>
-      </ComboboxContent>
-    </Combobox>
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Combobox items={Currencies}>
+        <ComboboxInput placeholder="Set currency" />
+        <ComboboxContent>
+          <ComboboxEmpty>No items found.</ComboboxEmpty>
+          <ComboboxList>
+            {(item) => (
+              <ComboboxItem key={item.value} value={item.value}>
+                {item.label}
+              </ComboboxItem>
+            )}
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
+    </SkeletonWrapper>
   );
 }

c3494b6dcf097526c1b5c33afe3874711edf130c
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 12:30:36 +0600

    feat: api

--- src/components/providers/RootProviders.tsx
+++ src/components/providers/RootProviders.tsx
@@ -1,20 +1,22 @@
-'use client'
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 interface RootProvidersProps {
   children: ReactNode;
 }
 
 const RootProviders = ({ children }: RootProvidersProps) => {
+  const [queryClient] = useState(() => new QueryClient({}));
   return (
-   <ThemeProvider
-   attribute="class"
-   defaultTheme='dark'
-   enableSystem
-   disableTransitionOnChange>
-    {children}
-   </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   );
 };
 

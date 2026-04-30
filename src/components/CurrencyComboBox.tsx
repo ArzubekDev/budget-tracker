26fa3929de32b33bf4ea370f3484fc927010bfdb
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 12:24:47 +0600

    feat: react-query

--- src/components/CurrencyComboBox.tsx
+++ src/components/CurrencyComboBox.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import {
   Combobox,
@@ -7,30 +7,23 @@ import {
   ComboboxInput,
   ComboboxItem,
   ComboboxList,
-} from "@/components/ui/combobox"
-
-const frameworks = [
-  "Next.js",
-  "SvelteKit",
-  "Nuxt.js",
-  "Remix",
-  "Astro",
-] as const
+} from '@/components/ui/combobox';
+import { Currencies } from '@/lib/currencies';
 
 export function CurrencyComboBox() {
   return (
-    <Combobox items={frameworks}>
-      <ComboboxInput placeholder="Select a framework" />
+    <Combobox items={Currencies}>
+      <ComboboxInput placeholder="Set currency" />
       <ComboboxContent>
         <ComboboxEmpty>No items found.</ComboboxEmpty>
         <ComboboxList>
           {(item) => (
-            <ComboboxItem key={item} value={item}>
-              {item}
+            <ComboboxItem key={item.value} value={item.value}>
+              {item.label}
             </ComboboxItem>
           )}
         </ComboboxList>
       </ComboboxContent>
     </Combobox>
-  )
+  );
 }

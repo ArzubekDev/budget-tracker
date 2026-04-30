0bacddef082af7f22b5447b8e439fc0df9ec1e58
ArzubekDev <arzubekmain@gmail.com>
2026-04-30 14:50:39 +0600

    feat: loading

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,27 @@
+import prisma from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
 
-const Page = () => {
+  const userSettings = await prisma.userSettings.findUnique({
+    where: { userId: user.id },
+  });
+  if (!userSettings) {
+    redirect('/wizard');
+  }
   return (
-    <section className="">
-      <h1>Page</h1>
-    </section>
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap justify-between items-center gap-6 py-8">
+<p className='font-bold text-3xl'>Hello, {user.firstName}! 👋</p>
+        </div>
+      </div>
+    </div>
   );
 };
 

e1d97c99912753410e5a5e66f51bab22811299e1
ArzubekDev <arzubekmain@gmail.com>
2026-05-01 11:07:01 +0600

    refac: CreateCategoryDialog

--- src/app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ src/app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -7,13 +7,21 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Popover, PopoverTrigger } from '@/components/ui/popover';
 import { TransactionType } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/categories';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { PlusSquare } from 'lucide-react';
+import { CircleOff, PlusSquare } from 'lucide-react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -53,7 +61,7 @@ const CreateCategoryDialog = ({ type }: CreateCategoryDialogProps) => {
         </DialogHeader>
         <Form {...form}>
           <form className="space-y-8">
-             <FormField
+            <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
@@ -63,6 +71,38 @@ const CreateCategoryDialog = ({ type }: CreateCategoryDialogProps) => {
                     <Input defaultValue={''} {...field} />
                   </FormControl>
                   <FormDescription>Transaction description (optional)</FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="icon"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Icon</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger>
+                        <Button variant={'outline'} className="h-25 w-full">
+                          {form.watch('icon') ? (
+                             <div className="flex flex-col items-center gap-2">
+                              <CircleOff className="h-12 w-12" />
+                              <span className='text-5xl' role='img'>{field.value}</span>
+                              <p className='text-xs text-muted-foreground'>Click to change</p>
+                            </div>
+                          ) : (
+                            <div className="flex flex-col items-center gap-2">
+                              <CircleOff className="h-12 w-12" />
+                              <p className='text-xs text-muted-foreground'>Click to select</p>
+                            </div>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                    </Popover>
+                  </FormControl>
+                  <FormDescription>
+                    This is how your category will appear in the app
+                  </FormDescription>
                 </FormItem>
               )}
             />

cb7e657a27f39d705341fe356015914f4e168e16
ArzubekDev <arzubekmain@gmail.com>
2026-05-01 10:33:56 +0600

    feat: schema/categories

---
+++ src/app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -0,0 +1,17 @@
+import { ReactNode, useState } from 'react';
+
+interface CreateCategoryDialogProps {
+  type: ReactNode;
+}
+
+const CreateCategoryDialog = ({ type }: CreateCategoryDialogProps) => {
+  const [open, setOpen] = useState();
+  
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default CreateCategoryDialog;

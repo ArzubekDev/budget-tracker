26a4d765d44c356fbe32492ed3168557f3777cee
ArzubekDev <arzubekmain@gmail.com>
2026-05-01 17:25:35 +0600

    feat: overview.ts

--- src/app/api/stats/balance/routes.ts
+++ src/app/api/stats/balance/routes.ts
@@ -1,3 +1,13 @@
-export async function GET(request: Request){
-    
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+export async function GET(request: Request) {
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const { searchParams } = new URL(request.url);
+  const from = searchParams.get('from');
+  const to = searchParams.get('to');
 }

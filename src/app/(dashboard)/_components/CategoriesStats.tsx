1edf9d374bcbe11db80cbecc9cdb0efe2ea5d940
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 15:30:17 +0600

    feat: categories/route.ts

---
+++ src/app/(dashboard)/_components/CategoriesStats.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { UserSettings } from '@/generated/client'
+import React from 'react'
+
+
+interface Props {
+    userSettings: UserSettings
+    from: Date,
+    to: Date
+}
+const CategoriesStats = ({userSettings, from, to}: Props) => {
+  return (
+    <div>CategoriesStats</div>
+  )
+}
+
+export default CategoriesStats

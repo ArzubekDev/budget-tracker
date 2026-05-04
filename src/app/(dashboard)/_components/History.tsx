7e482e12bc4d038b0a06f4da7ad46c66360da9f9
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 16:49:37 +0600

    feat: History

---
+++ src/app/(dashboard)/_components/History.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { UserSettings } from '@/generated/client'
+import React from 'react'
+
+const History = ({userSettings}: {userSettings: UserSettings}) => {
+    
+  return (
+    <div>History</div>
+  )
+}
+
+export default History

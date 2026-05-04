ddfa232c6e930222b44db0efe79b4257fecd64b6
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 17:11:13 +0600

    feat: hostory-periods

---
+++ src/app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Period, Timeframe } from '@/lib/types';
+
+interface Props {
+  period: Period;
+  setPeriod: (period: Period) => void;
+  timeframe: Timeframe;
+  setTimeframe: (timeframe: Timeframe) => void;
+}
+const HistoryPeriodSelector = ({period, timeframe, setPeriod, setTimeframe}: Props) => {
+  return <div>HistoryPeriodSelector</div>;
+};
+
+export default HistoryPeriodSelector;

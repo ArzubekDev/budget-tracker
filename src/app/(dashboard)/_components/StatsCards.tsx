72ef6f2f97006a37175634e8811730c9827f8ce0
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 14:22:29 +0600

    feat: Stats

--- src/app/(dashboard)/_components/StatsCards.tsx
+++ src/app/(dashboard)/_components/StatsCards.tsx
@@ -1,21 +1,34 @@
-import { GetBalanceStatsResponseType } from "@/app/api/stats/balance/routes";
-import { UserSettings } from "@/generated/client";
-import { useQuery } from "@tanstack/react-query";
+import { GetBalanceStatsResponseType } from '@/app/api/stats/balance/routes';
+import { UserSettings } from '@/generated/client';
+import { DateToUTCDate, GetFormatterForCurrency } from '@/lib/helpers';
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 
 interface Props {
-    from: Date,
-    to: Date,
-    userSettings: UserSettings
+  from: Date;
+  to: Date;
+  userSettings: UserSettings;
 }
 
-const StatsCards = ({from, to, userSettings}: Props) => {
-    const statsQuery = useQuery<GetBalanceStatsResponseType>({
-        queryKey: ["overview", "stats", from, to]
-    })
-  return (
-    <>
-    </>
-  );
+const StatsCards = ({ from, to, userSettings }: Props) => {
+  const statsQuery = useQuery<GetBalanceStatsResponseType>({
+    queryKey: ['overview', 'stats', from, to],
+    queryFn: () =>
+      fetch(`/api/stats/balance?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then((res) =>
+        res.json(),
+      ),
+  });
+
+  const formatter = useMemo(() => {
+    return GetFormatterForCurrency(userSettings.currency);
+  }, [userSettings.currency]);
+
+  const income = statsQuery.data?.income || 0;
+  const expense = statsQuery.data?.expense || 0;
+
+  const balance = income - expense;
+
+  return <div className='relative flex w-full flex-wrap gap-2 md:flex-nowrap'></div>;
 };
 
 export default StatsCards;

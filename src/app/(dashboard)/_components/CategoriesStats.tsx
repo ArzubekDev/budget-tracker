882f9d113bb94a29747a0de0edd6e59ce1718205
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 16:45:30 +0600

    feat: categoryStats

--- src/app/(dashboard)/_components/CategoriesStats.tsx
+++ src/app/(dashboard)/_components/CategoriesStats.tsx
@@ -3,6 +3,7 @@
 import { GetCategoriesStatsType } from '@/app/api/stats/categories/route';
 import SkeletonWrapper from '@/components/SkeletonWrapper';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { UserSettings } from '@/generated/client';
 import { DateToUTCDate, GetFormatterForCurrency } from '@/lib/helpers';
@@ -57,7 +58,9 @@ function CategoriesCard({
   return (
     <Card className="h-80 w-full col-span-6">
       <CardHeader>
-        <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col"></CardTitle>
+        <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col">
+          {type === "income" ? "Incomes" : "Expenses"} by category
+        </CardTitle>
       </CardHeader>
       <div className="flex items-center justify-between gap-2">
         {filteredData.length === 0 && (
@@ -65,7 +68,7 @@ function CategoriesCard({
             No data for the selected period
             <p className="text-sm text-muted-foreground">
               Try selecting a diferent period or try adding new{' '}
-              {type === 'income' ? 'incomes' : 'expenses'}
+              {type === 'income' ? 'Incomes' : 'Expenses'}
             </p>
           </div>
         )}
@@ -80,8 +83,11 @@ function CategoriesCard({
                     <div className="flex items-center justify-between">
                       <span className="flex items-center text-gray-400">
                         {item.categoryIcon} {item.category}
+                        <span className='ml-2 text-xs text-muted-foreground'>({percentage.toFixed(0)}%)</span>
                       </span>
+                      <span className='text-sm text-gray-400'>{formatter.format(amount)}</span>
                     </div>
+                    <Progress value={percentage} indicator={type === "income" ? "bg-emerald-500" : "bg-rose-500"}/>
                   </div>
                 );
               })}

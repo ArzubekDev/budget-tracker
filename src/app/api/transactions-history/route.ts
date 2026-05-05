bc5312575ce60a6d666039045f58fe632acde094
ArzubekDev <arzubekmain@gmail.com>
2026-05-05 15:22:44 +0600

    feat: transaction-history

---
+++ src/app/api/transactions-history/route.ts
@@ -0,0 +1,25 @@
+import { OverviewQuerySchema } from '@/schema/overview';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+export async function GET(request: Request) {
+  const user = currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const { searchParams } = new URL(request.url);
+  const from = searchParams.get('from');
+  const to = searchParams.get('to');
+
+  const queryParams = OverviewQuerySchema.safeParse({
+    from,
+    to,
+  });
+
+  if (!queryParams.success) {
+    return Response.json(queryParams.error.message, {
+      status: 400,
+    });
+  }
+}

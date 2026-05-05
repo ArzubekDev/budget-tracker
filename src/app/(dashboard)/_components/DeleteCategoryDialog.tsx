01c6519961427cf953ced29f6ede9fc594f1a495
ArzubekDev <arzubekmain@gmail.com>
2026-05-05 14:56:48 +0600

    fix: DeleteCategory

--- src/app/(dashboard)/_components/DeleteCategoryDialog.tsx
+++ src/app/(dashboard)/_components/DeleteCategoryDialog.tsx
@@ -1,17 +1,78 @@
-'use client'
+'use client';
 
-import { Category } from "@/generated/client";
-import { ReactNode } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Category } from '@/generated/client';
+import { TransactionType } from '@/lib/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { toast } from 'sonner';
+import { DeleteCategory } from '../_actions/categories';
 
 interface Props {
-    trigger: ReactNode;
-    category: Category
+  trigger: ReactNode;
+  category: Category;
 }
 
-const DeleteCategoryDialog = ({trigger, category}: Props) => {
+const DeleteCategoryDialog = ({ trigger, category }: Props) => {
+  const categoryIdentifier = `${category.name}-${category.type}`;
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: DeleteCategory,
+    onSuccess: async () => {
+      toast.success('Category deleted successfully!', {
+        id: categoryIdentifier,
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: ['categories'],
+      });
+    },
+    onError: () => {
+      toast.error('Something went error!', {
+        id: categoryIdentifier,
+      });
+    },
+  });
   return (
-    <div>DeleteCategoryDialog</div>
-  )
-}
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.This will parmenently delete your category
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading('Deleting category...', {
+                id: categoryIdentifier,
+              });
+              deleteMutation.mutate({
+                name: category.name,
+                type: category.type as TransactionType,
+              });
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
-export default DeleteCategoryDialog
+export default DeleteCategoryDialog;

55f15516ac6b444f1567dbefeaa8de768e06bed3
ArzubekDev <arzubekmain@gmail.com>
2026-05-05 16:51:55 +0600

    feat: FacetedFilters.tsx

--- src/app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ src/app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { GetTransactionsHistoryResponseType } from '@/app/api/transactions-history/route';
+import { DataTableColumnHeader } from '@/components/datatable/ColumnHeader';
 import SkeletonWrapper from '@/components/SkeletonWrapper';
 import {
   Table,
@@ -11,8 +12,17 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { DateToUTCDate } from '@/lib/helpers';
+import { cn } from '@/lib/utils';
 import { useQuery } from '@tanstack/react-query';
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useState } from 'react';
 
 interface Props {
   from: Date;
@@ -24,6 +34,7 @@ type TransactionHistoryRow = GetTransactionsHistoryResponseType[0];
 export const columns: ColumnDef<TransactionHistoryRow>[] = [
   {
     accessorKey: 'category',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Category" />,
     cell: ({ row }) => (
       <div className="flex gap2 capitalize">
         {row.original.categoryIcon}
@@ -31,8 +42,53 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
       </div>
     ),
   },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Description" />,
+    cell: ({ row }) => <div className="capitalize">{row.original.description}</div>,
+  },
+  {
+    accessorKey: 'date',
+    header: 'Date',
+    cell: ({ row }) => {
+      const date = new Date(row.original.date);
+      const formattedDate = date.toLocaleDateString('default', {
+        timeZone: 'UTC',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      });
+      return <div className="text-muted-foreground">{formattedDate}</div>;
+    },
+  },
+  {
+    accessorKey: 'type',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Type" />,
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          'capitalize rounded-lg text-center p-2',
+          row.original.type === 'income' && 'bg-emerald-400/10 text-emerald-500',
+          row.original.type === 'expense' && 'bg-rose-400/10 text-rose-500',
+        )}
+      >
+        {row.original.type}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'amount',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Amount" />,
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-center font-medium">
+        {row.original.formattedAmount}
+      </p>
+    ),
+  },
 ];
 const TransactionTable = ({ from, to }: Props) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const history = useQuery<GetTransactionsHistoryResponseType>({
     queryKey: ['transactions', 'history', from, to],
     queryFn: () =>
@@ -45,52 +101,57 @@ const TransactionTable = ({ from, to }: Props) => {
     data: history.data || emptyData,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
-   <div className='w-full'>
-    <div className='flex justify-between gap-2 flex-wrap items-end py-4'>TODO: Filters</div>
-     <SkeletonWrapper isLoading={history.isFetching}>
-      <div className="overflow-hidden rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
+    <div className="w-full">
+      <div className="flex justify-between gap-2 flex-wrap items-end py-4">TODO: Filters</div>
+      <SkeletonWrapper isLoading={history.isFetching}>
+        <div className="overflow-hidden rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    </SkeletonWrapper>
-   </div>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} className="h-24 text-center">
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </SkeletonWrapper>
+    </div>
   );
 };
 

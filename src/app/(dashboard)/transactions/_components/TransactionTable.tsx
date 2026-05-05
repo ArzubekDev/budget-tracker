77af5a16f0dc7169f871a58d0f25bdfd9600ab5f
ArzubekDev <arzubekmain@gmail.com>
2026-05-05 16:01:25 +0600

    fix: TransactionTable

--- src/app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ src/app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import { GetTransactionsHistoryResponseType } from '@/app/api/transactions-history/route';
+import SkeletonWrapper from '@/components/SkeletonWrapper';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { DateToUTCDate } from '@/lib/helpers';
 import { useQuery } from '@tanstack/react-query';
-import { ColumnDef } from '@tanstack/react-table';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 
 interface Props {
   from: Date;
   to: Date;
 }
-
+const emptyData: any = [];
 type TransactionHistoryRow = GetTransactionsHistoryResponseType[0];
 
 export const columns: ColumnDef<TransactionHistoryRow>[] = [
@@ -27,11 +36,59 @@ const TransactionTable = ({ from, to }: Props) => {
   const history = useQuery<GetTransactionsHistoryResponseType>({
     queryKey: ['transactions', 'history', from, to],
     queryFn: () =>
-      fetch(`/api/transactions-history?from=${DateToUTCDate(from)}to=${DateToUTCDate(to)}`).then(
+      fetch(`/api/transactions-history?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`).then(
         (res) => res.json(),
       ),
   });
-  return <div>TransactionTable</div>;
+
+  const table = useReactTable({
+    data: history.data || emptyData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <SkeletonWrapper isLoading={history.isFetching}>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </SkeletonWrapper>
+  );
 };
 
 export default TransactionTable;
